93df4266b5e8d5e7881ef6e1353b9aaae56da3ed
Victor <vmatsyshen@gmail.com>
2022-02-02 21:43:37 -0800

    added base react project

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Capstone Team! This is our basic React project.
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to automatically reload this page.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React 
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
