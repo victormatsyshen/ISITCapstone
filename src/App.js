7d1480bc527bbf130e198f4d2c1a83114ccfeba4
May Gaeul Kwon <44080658+GaeulKwon@users.noreply.github.com>
2022-02-07 15:55:24 -0800

    Adding May's Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import VictorsPage from './VictorsPage';
 import DylansPage from './DylansPage';
 import DevonsPage from './DevonsPage';
+import MaysPage from './MaysPage';
 import AppPage from './AppPage';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
@@ -21,6 +22,8 @@ function App() {
             <Route exact path='/victorspage' element={<VictorsPage />} />
             <Route exact path='/dylanspage' element={<DylansPage />} />
             <Route exact path='/devonspage' element={<DevonsPage />} />
+            <Route exact path='/mayspage' element={<MaysPage />} />
+
         </Routes>
 
         <MobileView>
