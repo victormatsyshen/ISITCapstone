8a1fa0208b6bcbafd1c25d60716deb2bb2a55702
Dylan York <dylanyork90@outlook.com>
2022-04-18 18:42:58 -0700

    Crocodile API

---
+++ CrocodileAPI/CrocodileAPI/Controllers/ItemController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace CrocodileAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ItemController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                           select Item_ID, Item_name,Item_type,Maker,Size_Dimension_Weight,Condition,Accesion_date,Collector,Item_narrative from
+                           dbo.Item
+
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ItemAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ CrocodileAPI/CrocodileAPI/Models/Item.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrocodileAPI.Models
+{
+    public class Item
+    {
+        public int ItemKey { get; set; }
+        public int Item_ID { get; set; }
+        public string Item_name { get; set; }
+        public string Item_type { get; set; }
+        public string Maker { get; set; }
+        public string Material { get; set; }
+        public string Size_Dimension_Weight { get; set; }
+        public string Condition { get; set; }
+        public string Accesion_date { get; set; }
+        public string Collector { get; set; }
+        public string Item_narrative { get; set; }
+        public string Item_Provenance { get; set; }
+        public string Cross_references { get; set; }
+
+
+    }
+}
