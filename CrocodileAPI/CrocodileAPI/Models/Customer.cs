6f0316272af9638579baac8a4e988c4cb62965d4
Dylan York <dylanyork90@outlook.com>
2022-05-23 17:53:01 -0700

    Added Model and Controller for Customer

---
+++ CrocodileAPI/CrocodileAPI/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrocodileAPI.Models
+{
+    public class Customer
+    {
+        public int CustomerKey { get; set; }
+        public string First_name { get; set; }
+        public string Last_name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
