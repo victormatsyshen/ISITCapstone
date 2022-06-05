fb124dc427400354215c52ba35c1196fccb9c62b
May Gaeul Kwon <44080658+GaeulKwon@users.noreply.github.com>
2022-06-05 15:04:04 -0700

    Add ExtraPage.js

---
+++ src/pages/ExtraPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Login from '../components/Login'
+
+export default function ExtraPage() {
+  return (
+    <div className={'homeContainer'}>
+      <Login />
+    </div>
+  )
+}
